dd7838552db43694cc80442015a597de0dbfc514
karlos191 <karelkubik191@gmail.com>
2024-10-20 15:09:53 +0200

    fix

--- auctions/tests.py
+++ auctions/tests.py
@@ -8,9 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 
 class AuctionTestCase(TestCase):
